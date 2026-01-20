2a5dbe402137ea26395caa45b76a15556bf75059
Alexandru Mihai Iura <alexiura93@yahoo.com>
2026-01-20 12:08:33 +0200

    feat(book): add read endpoints with availability

--- src/main/java/com/example/library/book/BookController.java
+++ src/main/java/com/example/library/book/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
